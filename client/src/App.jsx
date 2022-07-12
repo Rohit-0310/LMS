1f1e51129f229b3de0aa8aa939cfc478a6f98880
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-12 23:37:42 +0530

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import DashBoard from './Components/DashBoard';
+import Login from './Components/Login';
 
 function App() {
     return (
         <div>
-            <DashBoard />
+            {/* <DashBoard /> */}
+            <Routes>
+                <Route path={"/"} element={<DashBoard />}/>
+                <Route path={"/Login"} element={<Login />} />
+            </Routes>
         </div>
     );
 }
