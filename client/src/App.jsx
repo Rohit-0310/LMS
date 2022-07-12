a9efd65e33629a83d1e525e059daba593f2712bb
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-12 21:02:09 +0530

    Mui

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DashBoard from './Components/DashBoard';
+
+function App() {
+    return (
+        <div>
+            <DashBoard />
+        </div>
+    );
+}
+
+export default App;
