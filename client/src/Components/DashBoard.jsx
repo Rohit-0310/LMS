35013a47b420a447d681e395823391b47da08eff
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-14 21:59:58 +0530

    Dashboard ui update

--- client/src/Components/DashBoard.jsx
+++ client/src/Components/DashBoard.jsx
@@ -1,13 +1,26 @@
+import { Button } from '@mui/material'
 import React from 'react'
 import NavBar from './NavBar'
+import "./DashBoard.css"
+
 
 const DashBoard = () => {
-  return (
-    <div>
-        <NavBar />
-        <h2 style={{textAlign: 'center'}}>Dashboard </h2>
-    </div>
-  )
+    return (
+      <div>
+          <NavBar />
+            <div className="Dash_top">
+                <div className="Dash_refer">
+                  <div >Today's Schedule</div>
+                  <div className="Dash_button">
+                      <Button>BOOKMARKS</Button>
+                      <Button>REFER & EARN</Button>
+                  </div>
+                </div>
+            </div>
+          <hr />
+          {/* <h2 style={{textAlign: 'center'}}>Dashboard </h2> */}
+      </div>
+    )
 }
 
 export default DashBoard
