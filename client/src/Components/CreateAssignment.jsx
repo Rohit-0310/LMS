b3b932bb4987ae064ca09b9da34862afe8e82ea4
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-14 22:41:42 +0530

    Create lecture and Assignment ui

--- client/src/Components/CreateAssignment.jsx
+++ client/src/Components/CreateAssignment.jsx
@@ -8,7 +8,61 @@ const CreateAssignment = () => {
     return (
         <div>
             <NavBar />
-            <h1>Add Lecture Page</h1>
+
+<div className="ImgStyle">
+    <img  src="https://masai-lms-tau.vercel.app/assets/logo2.1d773fa0.png" alt="Masai School" />
+
+</div>
+
+
+<form >
+    <div className="fotm_main">
+        <h2>Add Assignment</h2>
+
+            <div className='InputBox'>
+                <label><b>Assignment Name</b></label> <br />
+                <input  type="text"  required />
+            </div>
+
+            <div className='InputBox'>
+                <label><b>Teacher Name</b></label> <br />
+                <input    />
+            </div>
+
+
+
+        <div className='InputBox'>
+            <label ><b>Assignment Type</b></label> <br />
+            <input  type="text"  required />
+        </div>
+
+        <div className='InputBox'>
+            <label ><b>Assignment Tag</b></label> <br />
+            <input  type="text"  required />
+        </div>
+
+        
+        <div className='InputBox'>
+            <label ><b>Status</b></label> <br />
+            <input  type="text"  required />
+        </div>
+
+        <div className='InputBox_name'>
+            <div className='InputBox'>
+                <label ><b>Date</b></label> <br />
+                <input  type="date"  required />
+            </div>
+            <div className='InputBox'>
+                <label ><b>Time</b></label> <br />
+                <input  type="Time"  required />
+            </div>
+        </div>
+
+
+        <button className="btn" type="submit" ><b>Create Assignments</b></button>
+
+    </div>
+</form>
         </div>
     )
 }
--- client/src/Components/CreateLecture.jsx
+++ client/src/Components/CreateLecture.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from './NavBar';
-import "./Admin.css";
+import "./Create.css";
 import { Button } from '@mui/material';
 
 
@@ -8,7 +8,58 @@ const CreateLecture = () => {
     return (
         <div>
             <NavBar />
-            <h1>Add Lecture Page</h1>
+
+            <div className="ImgStyle">
+                <img  src="https://masai-lms-tau.vercel.app/assets/logo2.1d773fa0.png" alt="Masai School" />
+
+            </div>
+            
+
+            <form >
+                <div className="fotm_main">
+                    <h2>Add Lecture</h2>
+
+                        <div className='InputBox'>
+                            <label><b>Lecture Name</b></label> <br />
+                            <input  type="text"  required />
+                        </div>
+
+                        <div className='InputBox'>
+                            <label><b>Teacher Name</b></label> <br />
+                            <input    />
+                        </div>
+
+
+
+                    <div className='InputBox'>
+                        <label ><b>Lecture Type</b></label> <br />
+                        <input  type="text"  required />
+                    </div>
+
+                    <div className='InputBox'>
+                        <label ><b>Lecture Tag</b></label> <br />
+                        <input  type="text"  required />
+                    </div>
+
+
+                    <div className='InputBox_name'>
+                        <div className='InputBox'>
+                            <label ><b>Date</b></label> <br />
+                            <input  type="date"  required />
+                        </div>
+
+
+                        <div className='InputBox'>
+                            <label ><b>Time</b></label> <br />
+                            <input  type="Time"  required />
+                        </div>
+                    </div>
+
+
+                    <button className="btn" type="submit" ><b>Create lecture</b></button>
+
+                </div>
+            </form>
         </div>
     )
 }
