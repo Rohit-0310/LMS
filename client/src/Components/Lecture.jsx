8cc279ce90d0485bb53560e94c0bb40c47dd8a21
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-15 00:53:22 +0530

    Lecture page done

--- client/src/Components/Lecture.jsx
+++ client/src/Components/Lecture.jsx
@@ -1,5 +1,7 @@
+import { Button } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import "./Lecture.css"
+import NavBar from './NavBar';
 
 
 
@@ -11,7 +13,7 @@ const Lecture = () => {
     const getData = () =>{
       fetch(`http://localhost:5000/lecture`)
         .then((data) =>data.json())
-        .then((res)=>{setLecture(res)})
+        .then((res)=>{setLecture(res.items)})
 
     }
 
@@ -21,9 +23,42 @@ const Lecture = () => {
     },[])
 
     console.log(lecture)
-  return (
-    <div>Lecture</div>
-  )
+    return (
+        <div>
+            <NavBar />
+                <div className="Lec_page">
+                    <h2 >Lectures</h2>
+                </div>
+                <hr />
+                <div>
+                <div className="Lec_page">{
+                    lecture.map((item)=>(
+                      <div className='card' key={item.id} >
+                        <div className="card_flex">
+                            <div style={{marginLeft:'20px'}}>
+                                <div style={{display: 'flex'}}>
+                                    <p style={{marginRight:'40px'}}>{item.lecturename}</p>
+                                    <p style={{backgroundColor:'green', color:'white', padding:'2px 10px 2px 10px'}}>{item.lecturetype}</p>
+                                </div>
+                                <div>
+                                    <span>{item.teachername} </span><span> Sceduled </span>
+                                    <span> {item.lecturetag} </span><span> at </span>
+                                    <span>{item.date}</span><span> - </span>
+                                    <span>{item.time}</span>
+                                </div>
+                            </div>
+                            <div style={{margin:'30px 40px'}}>
+                                <Button variant="contained" color="error">Absent</Button>
+                            </div>
+                        </div>
+                      </div>
+                    ))
+                }
+                </div>
+                </div>
+
+        </div>
+    )
 }
 
 export default Lecture
