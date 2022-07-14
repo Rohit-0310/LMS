e4d5a33cd413ec85fe3ef9948bcf9bb84112dbf0
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-14 23:34:11 +0530

    Add Assignment to DB

--- client/src/Components/CreateAssignment.jsx
+++ client/src/Components/CreateAssignment.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar';
 import "./Admin.css";
-import { Button } from '@mui/material';
 
 
 const CreateAssignment = () => {
+    const [form, setForm] = useState("");
+
+    const Handlechange = (e) => {
+        // console.log(e.target.value)
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+        console.log(value.length)
+    };
+    console.log(form)
+
+    const Handleclick = (e) => {
+        e.preventDefault();
+        // console.log(form);
+    
+        fetch(`http://localhost:5000/assignment`, {
+          method: "POST",
+          body: JSON.stringify(form),
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+
+        setTimeout(() => {
+            alert("Assignment Created successfully!")
+          }, 2000);
+    };
+
     return (
         <div>
             <NavBar />
+            <div className="Add_page">
+                  <h2 >Add Lecture Page</h2>
+            </div>
+            <hr />
 
 <div className="ImgStyle">
     <img  src="https://masai-lms-tau.vercel.app/assets/logo2.1d773fa0.png" alt="Masai School" />
@@ -21,45 +51,59 @@ const CreateAssignment = () => {
 
             <div className='InputBox'>
                 <label><b>Assignment Name</b></label> <br />
-                <input  type="text"  required />
+                <input  onChange={Handlechange} 
+                        name="assignmentname"
+                        type="text"  required />
             </div>
 
             <div className='InputBox'>
                 <label><b>Teacher Name</b></label> <br />
-                <input    />
+                <input  onChange={Handlechange} 
+                        name="teachername"
+                        type="text"  required />
             </div>
 
 
 
         <div className='InputBox'>
             <label ><b>Assignment Type</b></label> <br />
-            <input  type="text"  required />
+            <input  onChange={Handlechange} 
+                    name="assignmenttype"
+                    type="text"  required />
         </div>
 
         <div className='InputBox'>
             <label ><b>Assignment Tag</b></label> <br />
-            <input  type="text"  required />
+            <input  onChange={Handlechange} 
+                    name="assignmenttag"
+                    type="text"  required />
         </div>
 
         
         <div className='InputBox'>
             <label ><b>Status</b></label> <br />
-            <input  type="text"  required />
+            <input  onChange={Handlechange} 
+                    name="status"
+                    type="text"  required />
         </div>
 
         <div className='InputBox_name'>
             <div className='InputBox'>
                 <label ><b>Date</b></label> <br />
-                <input  type="date"  required />
+                <input  onChange={Handlechange} 
+                    name="date"
+                    type="date"  required />
             </div>
             <div className='InputBox'>
                 <label ><b>Time</b></label> <br />
-                <input  type="Time"  required />
+                <input  onChange={Handlechange} 
+                    name="time"
+                    type="Time"  required />
             </div>
         </div>
 
 
-        <button className="btn" type="submit" ><b>Create Assignments</b></button>
+        <button onClick={Handleclick} className="btn" type="submit" ><b>Create Assignments</b></button>
 
     </div>
 </form>
